f3a90de64720096e8f454b8867019364b48e27e7
Noi <noivuduc@gmail.com>
2021-07-18 00:54:36 +0800

    D Counting animals

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -24,5 +27,40 @@ public class Solution {
         shark.say();
         Dolphin dolphin = new Dolphin();
         dolphin.swim();
+
+        List<Animal> animals = new ArrayList<>(){{
+            add(new Chicken());
+            add(new Duck());
+            add(new Bird());
+            add(new Shark());
+        }};
+        countAnimal(animals);
+    }
+
+    private static void countAnimal(List<Animal> animals) {
+        int canFly = 0;
+        int canWalk = 0;
+        int canSing = 0;
+        int canSwim = 0;
+
+        for (Animal animal: animals) {
+            if (animal instanceof CanSwim) {
+                canSwim++;
+            }
+            if (animal instanceof CanFly) {
+                canFly++;
+            }
+            if (animal instanceof CanWalk) {
+                canWalk++;
+            }
+            if (animal instanceof CanSing) {
+                canSing++;
+            }
+        }
+
+        System.out.println(canFly + " animal(s) can fly");
+        System.out.println(canSing + " animal(s) can sing");
+        System.out.println(canSwim + " animal(s) can swim");
+        System.out.println(canWalk + " animal(s) can walk");
     }
 }
